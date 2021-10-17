011b040c2376123bf580e97161eb9ddae211bb9e
Napasorn Tanchittiwat <napasorn.tctw@gmail.com>
2021-10-17 12:50:27 +0700

    first commit

---
+++ studentjpa/StudentJPA.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentjpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author napas
+ */
+public class StudentJPA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student std = new Student(3, "Jane", 3.5);
+        persist(std);
+    }
+
+    public static void persist(Object object) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("StudentJPAPU");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+    
+    
+    
+}
